b49dbb135f395e77b1a3d57abb7c36caab79b2cc
frankcao@me.com Cao <frankcao@me.com>
2018-06-10 20:44:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name){
-  return `Happy ${holiday}, ${name}!`;
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(days,holiday) {
-  return 
+  return `It's ${days} days until ${holiday}!`
 }
